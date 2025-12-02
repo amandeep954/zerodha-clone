b75a142ce7848ebd5f4f796d228a6eb199e72a45
Aman Deep <ad707863@gmail.com>
2025-12-02 11:22:32 +0530

    Pricing & support section create

--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,6 +1,45 @@
 import React from "react";
+
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5 " id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="/">Track Tickets</a>
+      </div>
+      <div className="row p-5 mx-2">
+        <div className="col-7 " id="support1">
+          <h1 className="fs-3">
+            Search for an answer or browse help topics
+            <br />
+            to create a ticket
+          </h1>
+          <br />
+          <input placeholder="Eg. how do I activate F&O,why is my order rejected..." />
+          <br />
+          <br />
+          <a href="/">Track account opening</a>
+          <a href="/">Track segment activation</a>
+          <a href="/">Intraday</a>
+          <br />
+          <a href="/"> margins</a>
+          <a href="/">Kite user manual</a>
+        </div>
+
+        <div className="col-5" id="support2">
+          <h1 className="fs-3">Featured</h1>
+          <ol>
+            <li>
+              <a href="/">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="/">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
